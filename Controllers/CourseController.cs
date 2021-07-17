e43e0e4c7c79c9e5a072a069adbb4210e80689e2
ngusiroi245 <76592161+ngusiroi245@users.noreply.github.com>
2021-07-17 23:38:23 +0700

    Initial commit

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,47 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Category.ToList();
+            return View(objCourse);
+        }
+        [Authorize]
+        [ActionName("Create")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            //Ràng buộc
+            ModelState.Remove("LecturerId");
+            if (!ModelState.IsValid)
+            {
+                return View("Create");
+            }
+            //Lay login user ID
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().
+                FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            //Add vao csdl
+            context.Course.Add(objCourse);
+            context.SaveChanges();
+
+            //Tro ve Hom, Action Index
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,29 @@
+﻿namespace BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "LecturerId không được để trống!")]
+        [StringLength(128)]
+        public string LecturerId { get; set; }
+
+        [Required(ErrorMessage = "Place không được để trống!")]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Name;
+
+        public List<Category> ListCategory = new List<Category>();
+    }
+}
